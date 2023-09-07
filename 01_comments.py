45f749363c391d4a7bc1f24632eed24a7ec822e9
mukesh1996-ds <mks.mukesh1996@gmail.com>
2023-09-07 10:44:21 +0530

    updated

---
+++ 01_comments.py
@@ -0,0 +1,17 @@
+# Comments 
+
+# This is a single-line comment
+print("Hello, world!")  # This comment is at the end of the line
+
+'''
+This is a multi-line comment.
+You can use it to provide detailed explanations
+for your code.
+'''
+print("This is a multi-line comment example.")
+
+"""
+You can also use triple double-quotes for multi-line comments.
+Here's another example.
+"""
+print("Triple double-quotes can be used for multi-line comments as well.")
